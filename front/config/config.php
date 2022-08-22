3860664072d324b8810d7e779df8234f320bb320
sathish kumar <67854806+sathishds@users.noreply.github.com>
2022-08-23 00:17:35 +0530

    Query_string

--- front/config/config.php
+++ front/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'http://localhost/hkm/';
+// $config['base_url'] = 'http://localhost/hkmraipur_bhilai/';
 // $config['base_url'] = 'https://digitalmarketinggroup.in/harekrishna/hkm_new/';
 $config['base_url'] = 'https://hkmraipur-bhilai.org/';
 
@@ -54,7 +54,7 @@ $config['index_page'] = '';
 |
 | WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
 */
-$config['uri_protocol']	= 'REQUEST_URI';
+$config['uri_protocol']	= 'QUERY_STRING';
 
 /*
 |--------------------------------------------------------------------------
